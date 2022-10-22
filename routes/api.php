5e185100e065f777da877caa740c0bb336c5bd32
JC <ezjcd101@gmail.com>
2022-10-23 00:25:35 +0800

    added doctors crud

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,20 @@
 use App\Http\Controllers\API\StudentController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AppointmentController;
+use App\Http\Controllers\DoctorController;
 
 Route::get('/students', [StudentController::class, 'index']);
 Route::post('/add-student', [StudentController::class, 'store']);
 Route::get('/edit-student/{id}', [StudentController::class, 'edit']);
 Route::put('update-student/{id}', [StudentController::class, 'update']);
 Route::delete('delete-student/{id}', [StudentController::class, 'destroy']);
+
+Route::get('/doctors', [DoctorController::class, 'index']);
+Route::post('/add-doctor', [DoctorController::class, 'store']);
+Route::get('/edit-doctor/{id}', [DoctorController::class, 'edit']);
+Route::put('update-doctor/{id}', [DoctorController::class, 'update']);
+Route::delete('delete-doctor/{id}', [DoctorController::class, 'destroy']);
+
 
 Route::post('register',[UserController::class, 'register']);
 Route::post('login',[UserController::class, 'login']);
